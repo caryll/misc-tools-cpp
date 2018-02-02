262ff23baa0b4fd5c1044c150b68eba78e0cbdef
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-02-02 23:01:21 +0800

    Read ttf inputs.

--- glue-fonts/glue-fonts.cpp
+++ glue-fonts/glue-fonts.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include "bigendian.h"
-using ::std::cerr;
-using ::std::endl;
+#include "records.h"
+#include <fstream>
+#include <vector>
+#include <memory>
+using namespace std;
 
 int main(int argc, char **argv)
 {
@@ -9,6 +11,37 @@ int main(int argc, char **argv)
 	{
 		cerr << "Usage: glue-fonts output.ttc input1.ttf input2.ttf ..." << endl;
 		return -1;
+	}
+
+	TTCHeader ttc;
+	ttc.fonts.resize(argc - 2);
+
+	int index = 2;
+	for (auto& ttf : ttc.fonts)
+	{
+		char *filename = argv[index++];
+		ifstream input(filename, ifstream::binary);
+
+		if (!input.is_open())
+		{
+			cerr << "Cannot open file: " << filename << endl;
+			return -1;
+		}
+
+		input >> ttf;
+
+		ttf.tables.resize(ttf.numTables);
+		for (auto &t : ttf.tables)
+			input >> t;
+
+		for (auto &t : ttf.tables)
+			t.load(input);
+
+		if (input.fail())
+		{
+			cerr << "Error reading file: " << filename << endl;
+			return -1;
+		}
 	}
 
 	return 0;
