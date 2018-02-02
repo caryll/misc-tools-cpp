120d25e24e710ed90334566f85b4cffd85b33ab8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-02-02 23:20:22 +0800

    Fix to unsigned arthmatic.

--- glue-fonts/bigendian.h
+++ glue-fonts/bigendian.h
@@ -10,15 +10,15 @@ using ::std::ostream;
 
 inline uint16_t ReadBE16(istream& stream)
 {
-	char buff[2];
-	stream.read(buff, 2);
+	uint8_t buff[2];
+	stream.read(reinterpret_cast<char*>(buff), 2);
 	return (buff[0] << 8) + buff[1];
 }
 
 inline uint32_t ReadBE32(istream& stream)
 {
-	char buff[4];
-	stream.read(buff, 4);
+	uint8_t buff[4];
+	stream.read(reinterpret_cast<char*>(buff), 4);
 	return buff[3]
 		+ (buff[2] << 8)
 		+ (buff[1] << 16)
@@ -27,20 +27,20 @@ inline uint32_t ReadBE32(istream& stream)
 
 inline void WriteBE16(ostream& stream, uint16_t num)
 {
-	char buff[2];
+	uint8_t buff[2];
 	buff[0] = (num & 0xff00) >> 8;
 	buff[1] = num & 0x00ff;
-	stream.write(buff, 2);
+	stream.write(reinterpret_cast<char*>(buff), 2);
 }
 
 inline void WriteBE32(ostream& stream, uint32_t num)
 {
-	char buff[4];
+	uint8_t buff[4];
 	buff[0] = (num & 0xff000000) >> 24;
 	buff[1] = (num & 0x00ff0000) >> 16;
 	buff[2] = (num & 0x0000ff00) >> 8;
 	buff[3] = num & 0x000000ff;
-	stream.write(buff, 4);
+	stream.write(reinterpret_cast<char*>(buff), 4);
 }
 
 #endif
